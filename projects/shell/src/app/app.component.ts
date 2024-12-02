ca4bca7f518b3c7c6defbd66a0a33a568d0eb1c4
Sasa Gacik <sasa.gacik@seavus.com>
2024-12-02 10:04:50 +0100

    Updating project to serve shell as main

--- projects/shell/src/app/app.component.ts
+++ projects/shell/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BlogService } from 'shared-lib';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  providers: [BlogService],
+  template: `
+    <main class="container mx-auto p-4">
+      <router-outlet />
+    </main>
+  `
 })
-export class AppComponent {
-  title = 'shell';
-}
+export class AppComponent {}
--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'viewer',
+    loadChildren: () => import('../../../../projects/blog-viewer/src/app/app.routes').then(m => m.routes)
+  },
+  {
+    path: 'manager',
+    loadChildren: () => import('../../../../projects/blog-manager/src/app/app.routes').then(m => m.routes)
+  },
+  {
+    path: '',
+    redirectTo: 'viewer',
+    pathMatch: 'full'
+  }
+];
