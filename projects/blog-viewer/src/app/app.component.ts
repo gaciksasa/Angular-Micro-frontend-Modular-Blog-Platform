53d212938037aa4f7f32077ae9a3b75d87edf1be
Sasa Gacik <sasa.gacik@seavus.com>
2024-12-02 09:40:03 +0100

    Added tailwind css to app

--- projects/blog-viewer/src/app/app.component.ts
+++ projects/blog-viewer/src/app/app.component.ts
@@ -1,38 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { AsyncPipe } from '@angular/common';
-import { BlogService } from 'shared-lib';
-import { Observable } from 'rxjs';
-import { BlogPost } from 'shared-lib';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { PostListComponent } from './../../../blog-viewer/components/post-list/post-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AsyncPipe],
-  providers: [BlogService], 
+  imports: [RouterOutlet, PostListComponent],
   template: `
-    <div class="container mx-auto p-4">
-      <h1 class="text-2xl font-bold mb-6">Blog Posts</h1>
-      <div class="grid gap-4">
-      @if (posts$ | async; as posts) {
-        @for (post of posts; track post.postId) {
-        <div>
-          <div class="border p-4 rounded flex justify-between items-center hover:bg-gray-50">
-            <h3 class="text-xl font-semibold">{{ post.title }}</h3>
-            <p class="text-gray-600">{{ post.content }}</p>
-          </div>
-        </div>
-        }
-      }
-      </div>
-    </div>
+    <main class="container mx-auto p-4">
+      <app-post-list />
+    </main>
   `
 })
-export class AppComponent implements OnInit {
-  posts$!: Observable<BlogPost[]>;
-
-  constructor(private blogService: BlogService) {}
-
-  ngOnInit(): void {
-    this.posts$ = this.blogService.getPosts();
-  }
-}
+export class AppComponent {}
